488ca61244d8301046553d4db54e56e8ae793505
ric <ricardocarballo@gmail.com>
2012-12-07 08:45:39 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VoteMe::Application.config.secret_token = '5b08b7a6042062b3f2359b77abb6d0ef8714ad9b82452442b5e89e24007da2ca03c775f9a9fe72615788be2b5140eb52d96ab18eeef9769c02c5b5aa431653e9'
