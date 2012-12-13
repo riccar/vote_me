c7adb2308dc7fc0c2a39e844fb4c22b212523684
Ricardo Carballo <ricardocarballo@gmail.com>
2012-12-13 16:50:08 +1100

    micropost css change

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer          not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  votes      :integer          default(0)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Micropost < ActiveRecord::Base
   attr_accessible :content, :votes
   belongs_to :user
--- test/unit/micropost_test.rb
+++ test/unit/micropost_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer          not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  votes      :integer          default(0)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
