065e81b2b74c0dbaf5b9de9d047675742d04ce4d
ric <ricardocarballo@gmail.com>
2012-12-17 23:43:47 +1100

    voting

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,9 +1,27 @@
 class MicropostsController < ApplicationController
-  before_filter :signed_in_user, only: [:create, :destroy]
+  before_filter :signed_in_user, only: [:create, :destroy, :index, :vote]
 	
 	def index
-    @microposts = Micropost.all
-    #@micropost = current_user.microposts.build
+    @microposts_tmp = Micropost.order("votes DESC")
+    @microposts = []
+    count = 0
+    @microposts_tmp.each do |micropost|
+
+      if count < 5
+        @microposts[count] = micropost
+        count = count + 1
+      else
+        if @microposts[count - 1].votes  == micropost.votes
+          @microposts[count] = micropost
+          count = count + 1
+        else
+          break
+        end
+      end
+      
+    
+    end
+    #@microposts = @microposts_tmp
   end
 
   def create
@@ -24,10 +42,17 @@ class MicropostsController < ApplicationController
   end
 
   def vote
-    #@microposts = Micropost.all
     
-    #render  'microposts/index'
+    #find all of the microposts to add their current votes
+    @microposts_tmp = Micropost.all
+    @microposts_tmp.each do |micropost|
+      if params[:microposts].include?(micropost.id.to_s)
+        #check this
+       params[:microposts]["191"]["votes"] = params[:microposts]["191"]["votes"].to_i + 1 #micropost.votes.to_i
+      end
+    end
 
+    #update microposts
     @microposts = Micropost.update(params[:microposts].keys, params[:microposts].values).reject { |p| p.errors.empty? }
     if @microposts.empty?
       flash[:notice] = "Chinazos updated"
